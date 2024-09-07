b90f20831d0d49115d8d32a10af5bf445782b739
Axel Libori Roch <a.libori@gmail.com>
2024-09-07 17:34:02 +0200

    Add widgets

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Pages;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Pages\Dashboard as BaseDashboard;
+use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
+
+class Dashboard extends BaseDashboard
+{
+    use HasFiltersForm;
+
+    public function filtersForm(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->schema([
+                        DatePicker::make('startDate')->label(__('Start date')),
+                        DatePicker::make('endDate')->label(__('End date')),
+                    ])
+                    ->columns(3),
+            ]);
+    }
+}
---
+++ app/Filament/Widgets/BlogPostsOverview.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Widgets;
+
+use App\Enums\PostStatusEnum;
+use App\Models\Post;
+use DateTimeInterface;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
+use Illuminate\Database\Eloquent\Builder;
+
+class BlogPostsOverview extends StatsOverviewWidget
+{
+    use InteractsWithPageFilters;
+
+    protected static ?string $pollingInterval = null;
+
+    public function getStats(): array
+    {
+        /** @var DateTimeInterface|string|null $startDate */
+        $startDate = $this->filters['startDate'] ?? null;
+
+        /** @var DateTimeInterface|string|null $endDate */
+        $endDate = $this->filters['endDate'] ?? null;
+
+        return [
+            StatsOverviewWidget\Stat::make(
+                label: __('Total posts'),
+                value: Post::query()
+                    ->when($startDate, fn (Builder $query) => $query->whereDate('created_at', '>=', $startDate))
+                    ->when($endDate, fn (Builder $query) => $query->whereDate('created_at', '<=', $endDate))
+                    ->count(),
+            ),
+            StatsOverviewWidget\Stat::make(
+                label: __('Published posts'),
+                value: Post::query()
+                    ->when($startDate, fn (Builder $query) => $query->whereDate('created_at', '>=', $startDate))
+                    ->when($endDate, fn (Builder $query) => $query->whereDate('created_at', '<=', $endDate))
+                    ->where('status', PostStatusEnum::Published->value)
+                    ->count(),
+            )->color('success'),
+            StatsOverviewWidget\Stat::make(
+                label: __('Draft posts'),
+                value: Post::query()
+                    ->when($startDate, fn (Builder $query) => $query->whereDate('created_at', '>=', $startDate))
+                    ->when($endDate, fn (Builder $query) => $query->whereDate('created_at', '<=', $endDate))
+                    ->where('status', PostStatusEnum::Draft->value)
+                    ->count(),
+            )->color('warning'),
+            StatsOverviewWidget\Stat::make(
+                label: __('Reviewing posts'),
+                value: Post::query()
+                    ->when($startDate, fn (Builder $query) => $query->whereDate('created_at', '>=', $startDate))
+                    ->when($endDate, fn (Builder $query) => $query->whereDate('created_at', '<=', $endDate))
+                    ->where('status', PostStatusEnum::Reviewing->value)
+                    ->count(),
+            )->color('info'),
+        ];
+    }
+}
